c78c8b857d39684d4b8107d4eb592106cc83f081
Chobaka78 <34846941+Chobaka78@users.noreply.github.com>
2020-09-27 03:44:13 -0400

    Added website links

--- Bot.py
+++ Bot.py
@@ -1,5 +1,12 @@
+#/
 # Bot.py
 # Usman Farooqi
-import selenium
+# Sept, 27, 2020
+#/
+from selenium import webdriver
+PATH = "C:\Program Files (x86)\chromedriver.exe" # Location of the driver on local c drive (change to your pc)
 
-print("Just setting up the repo")
+driver = webdriver.Chrome(PATH)
+driver.get("https:google.com") # Link to th website that is going to be controlled by the bot 
+print(driver.title)
+driver.quit()
